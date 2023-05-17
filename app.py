dec264c42f56bc8bf6678aa790d7a32218e70120
Silas Nevstad <115965493+silasnevstad@users.noreply.github.com>
2023-05-17 18:46:54 -0400

    Updated

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# AI Writing Assistant
-# @SilasNevstad
+# AI Writing Assistant - @SilasNevstad
 import openai
 import requests
 import tiktoken
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS, cross_origin
 
 load_dotenv()
 
@@ -14,6 +14,8 @@ openai.api_key = OPENAI_API_KEY
 encoding = tiktoken.get_encoding("cl100k_base")
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 preset_messages = [
     {"role": "system", "content": "You are Writing Assistant GPT-4. Your job is to collaborate with a human partner in writing text, making their experience more efficient. Assist them by suggesting ideas, completing sentences, and providing context-appropriate phrases. Be mindful of grammar and coherence while maintaining a conversational tone. Always aim to understand the user\'s intention and provide relevant, helpful, and creative input. Try to match the human\'s tone so far and remember, your goal is to make writing easier by working together."},
@@ -21,6 +23,19 @@ preset_messages = [
 ]
 
 preset_message = "You are Writing Assistant GPT-4. Your job is to collaborate with a human partner in writing text, making their experience more efficient. Assist them by suggesting ideas, completing sentences, and providing context-appropriate phrases. Be mindful of grammar and coherence while maintaining a conversational tone. Always aim to understand the user's intention and provide relevant, helpful, and creative input. Try to match the human's tone so far and remember, your goal is to make writing easier by working together. \n\n In the next message, you will be given a part of the text the human is writing. It could be a sentence, a word, or even just a single letter. Based on that input, do your best to guess what the human wants to write and provide a suitable continuation. Note that you should only return the continuation you thought of, not any of the already written text.\n\nNow, your partner is writing: \""
+
+formalize_message = "Please formalize the following text: \""
+
+niceify_message = "Please make the following text sound and flow better, write it as well as you can: \""
+
+style_modes = {
+    0: "Your style should be casual and laid-back, like a text message to a close friend.",
+    1: "Your style should be friendly and approachable, as if you're communicating with a coworker.",
+    2: "Your style should be balanced, suitable for general purposes.",
+    3: "Your style should be formal and professional, like a business email.",
+    4: "Your style should be very formal and sophisticated, akin to an academic paper."
+}
+
 
 def calculate_tokens(text):
     return len(encoding.encode(text))
@@ -42,32 +57,118 @@ def prompt_gpt(prompt, model):
             messages = prompt)
         return response.choices[0].message.content
     except openai.OpenAIError as e:
-        return None
+        if model == 'gpt-4':
+            try:
+                response = openai.ChatCompletion.create(
+                    model='gpt-3.5-turbo',
+                    messages = prompt)
+                return response.choices[0].message.content
+            except openai.OpenAIError as e:
+                print(e)
+                return None
+        else:
+            print(e)
+            return None
     
-def ai_assist(text):
-    prompt = "Now, your partner is writing: \"" + text + "\"\n\n"
+def ai_assist(text, style):
+    style_prompt = style_modes.get(style, "Your style should be balanced, suitable for general purposes.")
+    prompt = "Now, your partner is writing: \"" + text + "\"\n\n" + style_prompt
     messages = preset_messages + [{"role": "system", "content": prompt}]
     response = prompt_gpt(messages, "gpt-4")
-    
     return response
+
+def gpt_from_preset_and_text_and_style(preset, text, style):
+    style_prompt = style_modes.get(style, "Your style should be balanced, suitable for general purposes.")
+    prompt = preset + text + "\"\n\n" + style_prompt
+    response = prompt_gpt([{"role": "system", "content": prompt}], "gpt-4")
+    return response
+
+def formalize_text(text, style):
+    return gpt_from_preset_and_text_and_style(formalize_message, text, style)
+
+def niceify_text(text, style):
+    return gpt_from_preset_and_text_and_style(niceify_message, text, style)
     
-@app.route('/buddy', methods=['GET'])
+@app.route('/v1/buddy', methods=['POST', 'OPTIONS'])
+@cross_origin()
 def writing_assistant():
-    text = request.args.get('text')
+    data = request.get_json()
+    text = data.get('text')
+    style = data.get('style', 2)  # default to balanced style if not provided
     
     if text == None:
         return jsonify({
-            'success': False,
             'error': 'No text provided'
         }), 400
         
     text = limit_tokens(text, (8192 - 190)) # 185 is the tokens from the preset message
         
-    continuation = ai_assist(text)
+    continuation = ai_assist(text, style)
     
     return jsonify({
-        'success': True,
-        'response': continuation
+        'suggestion': continuation
+    }), 200
+
+@app.route('/v1/formalize', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def formalize():
+    data = request.get_json()
+    text = data.get('text')
+    style = data.get('style', 2)  # default to balanced style if not provided
+    
+    if text == None:
+        return jsonify({
+            'error': 'No text provided'
+        }), 400
+        
+    text = limit_tokens(text, (8192 - 190))
+    
+    formalized = formalize_text(text, style)
+    
+    return jsonify({
+        'response': formalized
+    }), 200
+
+@app.route('/v1/niceify', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def niceify():
+    data = request.get_json()
+    text = data.get('text')
+    style = data.get('style', 2)  # default to balanced style if not provided
+    
+    if text == None:
+        return jsonify({
+            'error': 'No text provided'
+        }), 400
+        
+    text = limit_tokens(text, (8192 - 190))
+    
+    niceified = niceify_text(text, style)
+    
+    return jsonify({
+        'response': niceified
+    }), 200
+    
+@app.route('/v1/auto', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def auto():
+    data = request.get_json()
+    text = data.get('text')
+    style = data.get('style', 2)  # default to balanced style if not provided
+    
+    if text == None:
+        return jsonify({
+            'error': 'No text provided'
+        }), 400
+        
+    text = limit_tokens(text, (8192 - 190))
+    style_prompt = "Make sure your response is the following style: " + style_modes.get(style, "Your style should be balanced, suitable for general purposes.")
+    prompt = text + "\"\n\n" + style_prompt
+    
+    gpt_response = prompt_gpt([{"role": "system", "content": text}], "gpt-3.5-turbo")
+    
+    return jsonify({
+        'response': gpt_response
     }), 200
 
 if __name__ == '__main__':
